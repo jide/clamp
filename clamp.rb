550e40a7c7fd4a8c3f7ccbcedd892eb172496bce
Julien De Luca <jide@air-de-julien.home>
2014-11-20 11:27:48 +0100

    Homebrew support

--- clamp.rb
+++ clamp.rb
@@ -4,7 +4,7 @@ class Clamp < Formula
   homepage "http://jide.github.io/clamp"
   url "https://github.com/jide/clamp/archive/1.0.tar.gz"
   version "1.0"
-  md5 "2132e20b1b9af90f94d8d24fbc41bae1"
+  sha1
 
   depends_on "mariadb"
 
