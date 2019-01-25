9f6f21d57d0c7041972ce2414dfdaa67dcecc120
Andrew Erickson <aerickson@gmail.com>
2019-01-25 01:14:21 -0800

    tweak warning conditions and text

--- includes/Clamp/StartCommand.php
+++ includes/Clamp/StartCommand.php
@@ -8,13 +8,13 @@ class StartCommand extends \Clamp\Command
 {
     public function execute(array $args = array(), array $options = array())
     {
-        # if we're on <10.14, check if port is 80 and warn that it probably won't work
+        # if we're on <10.14 and port is 80, warn that this probably won't work
         # and that they should change to a non-priviledged port.
         $version = substr(php_uname('r'), 0, 2);
         $port = $this->getConfig('$.apache.options.listen');
-        if($version < 18 && $port == 80) {
+        if($version < 18 && $port < 1024) {
             print("WARNING: Mac OS X 10.13 and lower requires sudo to run on ports lower than 1024.\n");
-            print("         It's recommended that you select a port higher than 1024.\n");
+            print("         You should probably select a port higher than 1023.\n");
             print("         This command will likely fail.\n");
             print("\n");
         }
