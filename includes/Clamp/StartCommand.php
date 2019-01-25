ddbc60c7c6e7e41a2a8fe752b317b7487fbe59c6
Andrew Erickson <aerickson@gmail.com>
2019-01-25 02:11:11 -0800

    fix spelling error

--- includes/Clamp/StartCommand.php
+++ includes/Clamp/StartCommand.php
@@ -9,7 +9,7 @@ class StartCommand extends \Clamp\Command
     public function execute(array $args = array(), array $options = array())
     {
         # if we're on <10.14 and port is 80, warn that this probably won't work
-        # and that they should change to a non-priviledged port.
+        # and that they should change to a non-privileged port.
         $version = substr(php_uname('r'), 0, 2);
         $port = $this->getConfig('$.apache.options.listen');
         if($version < 18 && $port < 1024) {
@@ -17,7 +17,7 @@ class StartCommand extends \Clamp\Command
 WARNING: Mac OS X 10.13 and lower requires sudo to run on ports lower than 1024.
          Please select a port higher than 1023 (currently using port $port).
          This command will likely fail.
-         
+
 EOT;
             $this->writeln($warning, ConsoleKit\Colors::RED);
         }
