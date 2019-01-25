172a2c18d64bc296fa72de98376375653a9b5492
Andrew Erickson <aerickson@gmail.com>
2019-01-25 00:03:40 -0800

    todo

--- includes/Clamp/StartCommand.php
+++ includes/Clamp/StartCommand.php
@@ -8,6 +8,8 @@ class StartCommand extends \Clamp\Command
 {
     public function execute(array $args = array(), array $options = array())
     {
+        # TODO: if we're on <10.14, check if port is 80 and warn that it probably won't work
+        #       and that they should change to a non-priviledged port.
         $this->getConsole()->execute('apache', array('start'), array_merge($this->getConfig('$.apache.options'), $options));
         $this->getConsole()->execute('host', array('set'), array_merge($this->getConfig('$.host.options'), $options));
         $this->getConsole()->execute('mysql', array('start'), array_merge($this->getConfig('$.mysql.options'), $options));
