22095b7774813059a5b07350d45a43344a565270
Andrew Erickson <aerickson@gmail.com>
2019-01-27 23:43:47 -0800

    tests say what they're doing

--- .travis/test_travis.sh
+++ .travis/test_travis.sh
@@ -14,23 +14,23 @@ set -e
 # cleanup any lingering children
 pkill httpd | true
 
-echo -n "Test 1: "
+echo -n "Test 1: Checking that command exited normally..."
 if [ $retVal != 124 ]; then
   echo "Non-124 exit code ($retVal)!"
   exit 1
 fi
-echo "OK"
+echo " OK"
 
-echo -n "Test 2: "
+echo -n "Test 2: Checking that Apache started..."
 if [[ "$OUTPUT" != *"Apache server started"* ]]; then
   echo "ERROR: Apache didn't seem to start!"
   exit 1
 fi
-echo "OK"
+echo " OK"
 
-echo -n "Test 3: "
+echo -n "Test 3: Checking that MySQL started..."
 if [[ "$OUTPUT" != *"MySQL server started"* ]]; then
   echo "ERROR: MySQL didn't seem to start!"
   exit 1
 fi
-echo "OK"
+echo " OK"
