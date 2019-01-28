64c303ddae5a9b2bd41e0318ee4b7f7d330db33e
Andrew Erickson <aerickson@gmail.com>
2019-01-27 17:10:09 -0800

    host: only run if we're setting something other than localhost

--- includes/Clamp/HostCommand.php
+++ includes/Clamp/HostCommand.php
@@ -12,25 +12,31 @@ class HostCommand extends \Clamp\Command
 
     public function executeSet(array $args = array(), array $options = array())
     {
-        if (!shell_exec('grep -R "' . $this->buildParameters($options) . '" /etc/hosts')) {
-            exec('echo "' . $this->buildParameters($options) . '" | sudo tee -a /etc/hosts');
-            exec('dscacheutil -flushcache');
-            $this->writeln('Host set', ConsoleKit\Colors::GREEN);
-        }
-        else {
-            $this->writeln('Host is already set', ConsoleKit\Colors::YELLOW);
+        $host = reset($this->getConfig("$.host.options"));
+        if($host != 'localhost') {
+            if (shell_exec('grep -R "' . $this->buildParameters($options) . '" /etc/hosts')) {
+                exec('echo "' . $this->buildParameters($options) . '" | sudo tee -a /etc/hosts');
+                exec('dscacheutil -flushcache');
+                $this->writeln('Host set', ConsoleKit\Colors::GREEN);
+            }
+            else {
+                $this->writeln('Host is already set', ConsoleKit\Colors::YELLOW);
+            }
         }
     }
 
     public function executeUnset(array $args = array(), array $options = array())
     {
-        if (shell_exec('grep -R "' . $this->buildParameters($options) . '" /etc/hosts')) {
-            exec('sudo sed -i "" -e "/' . preg_quote($this->buildParameters($options)) . '/d" /etc/hosts');
-            exec('dscacheutil -flushcache');
-            $this->writeln('Host unset', ConsoleKit\Colors::RED);
-        }
-        else {
-            $this->writeln('Host is not set', ConsoleKit\Colors::YELLOW);
+        $host = reset($this->getConfig("$.host.options"));
+        if($host != 'localhost') {
+            if (shell_exec('grep -R "' . $this->buildParameters($options) . '" /etc/hosts')) {
+                exec('sudo sed -i "" -e "/' . preg_quote($this->buildParameters($options)) . '/d" /etc/hosts');
+                exec('dscacheutil -flushcache');
+                $this->writeln('Host unset', ConsoleKit\Colors::RED);
+            }
+            else {
+                $this->writeln('Host is not set', ConsoleKit\Colors::YELLOW);
+            }
         }
     }
 }
