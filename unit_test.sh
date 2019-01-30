fc8d82ad80360a10d86d692e90b7fdb7694bee8d
Andrew Erickson <aerickson@gmail.com>
2019-01-30 00:55:52 -0800

    quiet up unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -3,6 +3,6 @@
 set -e
 
 # quiet
-# ./vendor/bin/phpunit --bootstrap vendor/autoload.php tests
+./vendor/bin/phpunit --bootstrap vendor/autoload.php tests
 # verbose
-./vendor/bin/phpunit --bootstrap vendor/autoload.php -v --debug tests
+# ./vendor/bin/phpunit --bootstrap vendor/autoload.php -v --debug tests
