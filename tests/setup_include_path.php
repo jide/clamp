25f9c85731bf2969e343c49b0466aa8d331d6951
Andrew Erickson <aerickson@gmail.com>
2019-01-30 00:42:31 -0800

    cleanup

--- tests/setup_include_path.php
+++ tests/setup_include_path.php
@@ -2,8 +2,8 @@
 
 $paths = array(
     realpath(__DIR__ . DIRECTORY_SEPARATOR .  '../includes'),
-    realpath(__DIR__ . DIRECTORY_SEPARATOR .  '../includes/ConsoleKit/src'),
-    realpath(__DIR__ . DIRECTORY_SEPARATOR . '../includes/Clamp')
+    // realpath(__DIR__ . DIRECTORY_SEPARATOR .  '../includes/ConsoleKit/src'),
+    // realpath(__DIR__ . DIRECTORY_SEPARATOR . '../includes/Clamp')
 );
 
 set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $paths));
