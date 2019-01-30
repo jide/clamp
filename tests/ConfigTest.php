3608f649e47cd866dd0f751345db3b5019b8fc42
Andrew Erickson <aerickson@gmail.com>
2019-01-30 00:42:05 -0800

    further even still

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -3,14 +3,12 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-// require_once('setup_include_path.php');
+require_once('setup_include_path.php');
 
 require_once realpath(__DIR__ . '/../vendor/autoload.php');
-// require('ConfigOptionsParser.php');
 
 use Clamp\ConfigOptionsParser;
 use JsonPath\JsonPath;
-// use ConsoleKit\DefaultOptionsParser;
 
 class ConfigTest extends TestCase
 {
